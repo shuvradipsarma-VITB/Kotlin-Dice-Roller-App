b5f170a4dcbe6c9515be0385755277236a124096
Shuvradip Sarma <129774964+shuvradipsarma-VITB@users.noreply.github.com>
2023-11-09 17:05:42 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    implementation ("androidx.activity:activity-ktx:1.3.1")
+
+
+
 }
